95bae42dcd787c363eea0ceba6204420c6df33b2
Yamama Alsaid Hasan <yamamaalsaidhasan@gmail.com>
2026-08-18 21:10:12 +0300

    Resolved merge conflicts with origin main

--- lib/constance/strings.dart
+++ lib/constance/strings.dart
@@ -1,6 +1,6 @@
 
 final String localIp = '192.168.1.104';
 
- final baseUrl = 'http://$localIp:3000/';
+//  final baseUrl = 'http://$localIp:3000/';
 
-// final baseUrl = "https://spied-wifi-specked.ngrok-free.dev/";
+final baseUrl = "https://spied-wifi-specked.ngrok-free.dev/";
